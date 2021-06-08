300184e006241e105a40e4f66d1107a11c536823
asif-kamal <asif.kamal1489@gmail.com>
2021-06-08 10:35:09 -0400

    Added InputField component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import InputField from './components/InputField';
 
 function App() {
   return (
     <div className="App">
-     
+     <h1>Todo List</h1>
+     <InputField />
     </div>
   );
 }
