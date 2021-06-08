fce3897029a49907f5e19e4bfbc6515f462d4cda
asif-kamal <asif.kamal1489@gmail.com>
2021-06-08 12:40:53 -0400

    Added handleSubmit and handleChange on InputField.

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -2,10 +2,20 @@ import React from 'react';
 
 
 class InputField extends React.Component {
+
+    state = {
+        todoItem: "",
+    }
+
+    handlechange = e => {this.setState({todoItem: e.target.value})}
+    handleSubmit = e => {
+        e.preventDefault();
+    }
+
     render() {
         return(
             <form>
-                <input type='text' name='todo' placeholder='Add a todo item to your list'/>
+                <input type='text' name='todoItem' placeholder='Add a todo item to your list' onChange={this.handleChange}/>
             </form>
         )
     }
